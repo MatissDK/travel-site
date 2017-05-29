94fea1dcf349cf2a46df5fd82394fd2982eff8a2
Otto Matiss <matiss.aprups@gmail.com>
2017-05-29 23:24:57 +0300

    Gulp css setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
 
-
+//this is default task/ it can run by command gulp
 gulp.task('default', function () {
     console.log('Gulp task created');
 });
@@ -11,10 +15,15 @@ gulp.task('html', function () {
 });
 
 gulp.task('styles', function () {
-    console.log('Styles gulp');
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([nested,cssvars,autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch',function () {
+
+
+
+gulp.task('watch', function () {
     watch('./app/index.html', function () {
         gulp.start('html');
     });
